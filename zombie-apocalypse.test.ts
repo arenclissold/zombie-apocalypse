80a27f439e1010ec4302a015d20be1c7597589c7
Aren Clissold <arenclissold@gmail.com>
2022-07-07 09:27:23 +1000

    created basic zombie movement and passed associated tests

--- zombie-apocalypse.test.ts
+++ zombie-apocalypse.test.ts
@@ -1,10 +1,28 @@
 import { Apocolypse } from "./zombie-apocalypse"
 
 describe('Apocalypse', () => {
-  describe('Create grid', () => {
+  describe('initialize', () => {
     it('has dimensions n for the given input', () => {
-      const apocalypse = new Apocolypse(5)
-      expect(apocalypse.gridDimensions).toEqual(5)
+      const apocalypse = new Apocolypse(4, {x: 3, y: 1}, [{x: 0, y: 1}, {x: 1, y: 2}, {x: 1, y: 1}], 'RDRU' )
+      expect(apocalypse.gridDimensions).toEqual(4)
+    })
+
+    it('has zombie starting position equal to the input starting position', () => {
+      const apocalypse = new Apocolypse(4, {x: 3, y: 1}, [{x: 0, y: 1}, {x: 1, y: 2}, {x: 1, y: 1}], 'RDRU' )
+      expect(apocalypse.movingZombieLocation).toEqual({x: 3, y: 1})
+    })
+
+    it('has creature starting positions equal to the input starting positions', () => {
+      const apocalypse = new Apocolypse(4, {x: 3, y: 1}, [{x: 0, y: 1}, {x: 1, y: 2}, {x: 1, y: 1}], 'RDRU' )
+      expect(apocalypse.creatureLocations).toEqual([{x: 0, y: 1}, {x: 1, y: 2}, {x: 1, y: 1}])
+    })
+  })
+
+  describe('Movement', () => {
+    it('moves the zombie to the correct location, inside the given grid dimensions', () => {
+      const apocalypse = new Apocolypse(4, {x: 3, y: 1}, [], 'RDRU' )
+      apocalypse.evaluate()
+      expect(apocalypse.movingZombieLocation).toEqual({x: 1, y: 1})
     })
   })
 })
--- zombie-apocalypse.ts
+++ zombie-apocalypse.ts
@@ -1,6 +1,64 @@
+interface Location {
+  x: number;
+  y: number;
+}
+type Instruction = 'U' | 'D' | 'L' | 'R'
+
 export class Apocolypse {
   gridDimensions: number
-  constructor(gridDimensions: number) {
+  movingZombieLocation: Location
+  creatureLocations: Location[]
+  instructions: Instruction[]
+
+  constructor(
+    gridDimensions: number,
+    zombieStart: Location,
+    creatureLocations: Location[],
+    instructions: any
+  )
+  {
     this.gridDimensions = gridDimensions
+    this.movingZombieLocation = zombieStart
+    this.creatureLocations = creatureLocations
+    this.instructions = instructions.split('')
+  }
+
+  createZombie(): void {
+
+  }
+
+  evaluate(): void {
+    this.instructions.forEach((instruction: Instruction) => {
+      // move zombie
+      switch (instruction) {
+        case 'U':
+          this.movingZombieLocation.y += 1
+          break;
+        case 'D':
+          this.movingZombieLocation.y -= 1
+          break;
+        case 'L':
+          this.movingZombieLocation.x -= 1
+          break;
+        case 'R':
+          this.movingZombieLocation.x += 1
+          break;
+      }
+
+      // check if zombie has gone out of bounds and correct location if it has
+      if (this.movingZombieLocation.x < 0) {
+        this.movingZombieLocation.x += this.gridDimensions
+      } else if (this.movingZombieLocation.y < 0) {
+        this.movingZombieLocation.y += this.gridDimensions
+      } else if (this.movingZombieLocation.x > this.gridDimensions) {
+        this.movingZombieLocation.x -= this.gridDimensions
+      } else if (this.movingZombieLocation.y > this.gridDimensions) {
+        this.movingZombieLocation.y -= this.gridDimensions
+      }
+      // check if in creature location
+      if (this.creatureLocations.includes(this.movingZombieLocation)) {
+
+      }
+    })
   }
 }
