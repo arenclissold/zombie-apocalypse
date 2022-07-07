d68bc678a44c295d7ecf8fc3d672f1606adb72c0
Aren Clissold <arenclissold@gmail.com>
2022-07-07 12:45:18 +1000

    added conversion of creatures into zombies and iterating over multiple zombies

--- zombie-apocalypse.test.ts
+++ zombie-apocalypse.test.ts
@@ -3,26 +3,46 @@ import { Apocolypse } from "./zombie-apocalypse"
 describe('Apocalypse', () => {
   describe('initialize', () => {
     it('has dimensions n for the given input', () => {
-      const apocalypse = new Apocolypse(4, {x: 3, y: 1}, [{x: 0, y: 1}, {x: 1, y: 2}, {x: 1, y: 1}], 'RDRU' )
+      const apocalypse = new Apocolypse(4, [3, 1], [], '' )
       expect(apocalypse.gridDimensions).toEqual(4)
     })
 
     it('has zombie starting position equal to the input starting position', () => {
-      const apocalypse = new Apocolypse(4, {x: 3, y: 1}, [{x: 0, y: 1}, {x: 1, y: 2}, {x: 1, y: 1}], 'RDRU' )
-      expect(apocalypse.movingZombieLocation).toEqual({x: 3, y: 1})
+      const apocalypse = new Apocolypse(4, [3, 1], [], '' )
+      expect(apocalypse.zombies[0].location).toEqual([3, 1])
     })
 
     it('has creature starting positions equal to the input starting positions', () => {
-      const apocalypse = new Apocolypse(4, {x: 3, y: 1}, [{x: 0, y: 1}, {x: 1, y: 2}, {x: 1, y: 1}], 'RDRU' )
-      expect(apocalypse.creatureLocations).toEqual([{x: 0, y: 1}, {x: 1, y: 2}, {x: 1, y: 1}])
+      const apocalypse = new Apocolypse(4, [3, 1], [[0, 1], [1, 2], [1, 1]], '' )
+      expect(apocalypse.creatureLocations).toEqual([[0, 1], [1, 2], [1, 1]])
     })
   })
 
   describe('Movement', () => {
     it('moves the zombie to the correct location, inside the given grid dimensions', () => {
-      const apocalypse = new Apocolypse(4, {x: 3, y: 1}, [], 'RDRU' )
+      const apocalypse = new Apocolypse(4, [3, 1], [], 'RDRU' )
       apocalypse.evaluate()
-      expect(apocalypse.movingZombieLocation).toEqual({x: 1, y: 1})
+      expect(apocalypse.zombies[0].location).toEqual([1, 1])
+    })
+
+    it('creates zombies for creatures that are passed through', () => {
+      const apocalypse = new Apocolypse(4, [3, 1], [[0, 1], [1, 2], [1, 1]], 'RDRU' )
+      apocalypse.evaluate()
+      expect(apocalypse.zombies.length).toEqual(4)
+    })
+
+    it('creatures are removed from creatureLocations once turned into a zombie', () => {
+      const apocalypse = new Apocolypse(4, [3, 1], [[0, 1], [1, 2], [1, 1]], 'RDRU' )
+      apocalypse.evaluate()
+      expect(apocalypse.creatureLocations.length).toEqual(0)
+    })
+
+    it('new zombies move to their correct locations', () => {
+      const apocalypse = new Apocolypse(4, [3, 1], [[0, 1], [1, 2], [1, 1]], 'RDRU' )
+      apocalypse.evaluate()
+      expect(apocalypse.zombies[1].location).toEqual([2, 1])
+      expect(apocalypse.zombies[2].location).toEqual([3, 2])
+      expect(apocalypse.zombies[3].location).toEqual([3, 1])
     })
   })
 })
--- zombie-apocalypse.ts
+++ zombie-apocalypse.ts
@@ -1,12 +1,17 @@
-interface Location {
-  x: number;
-  y: number;
-}
+type Location = [number, number]
 type Instruction = 'U' | 'D' | 'L' | 'R'
+
+export class Zombie {
+  location: Location
+
+  constructor(location: Location) {
+    this.location = location
+  }
+}
 
 export class Apocolypse {
   gridDimensions: number
-  movingZombieLocation: Location
+  zombies: Zombie[]
   creatureLocations: Location[]
   instructions: Instruction[]
 
@@ -18,47 +23,76 @@ export class Apocolypse {
   )
   {
     this.gridDimensions = gridDimensions
-    this.movingZombieLocation = zombieStart
     this.creatureLocations = creatureLocations
     this.instructions = instructions.split('')
+    this.zombies = [new Zombie(zombieStart)]
   }
 
-  createZombie(): void {
-
+  createZombie(creatureLocation: Location): void {
+    const creatureLocationCopy: Location = [...creatureLocation]
+    this.zombies.push(new Zombie(creatureLocationCopy))
   }
 
   evaluate(): void {
-    this.instructions.forEach((instruction: Instruction) => {
-      // move zombie
-      switch (instruction) {
-        case 'U':
-          this.movingZombieLocation.y += 1
-          break;
-        case 'D':
-          this.movingZombieLocation.y -= 1
-          break;
-        case 'L':
-          this.movingZombieLocation.x -= 1
-          break;
-        case 'R':
-          this.movingZombieLocation.x += 1
-          break;
-      }
+    // iterate over zombies, a for loop must be used here as we are adding to the array while iterating
+    let zombieIndex = 0
+    for (let zombie of this.zombies) {
+      this.instructions.forEach((instruction: Instruction) => {
+        // move zombie
+        switch (instruction) {
+          case 'U':
+            zombie.location[1] -= 1
+            break;
+          case 'D':
+            zombie.location[1] += 1
+            break;
+          case 'L':
+            zombie.location[0] -= 1
+            break;
+          case 'R':
+            zombie.location[0] += 1
+            break;
+        }
 
-      // check if zombie has gone out of bounds and correct location if it has
-      if (this.movingZombieLocation.x < 0) {
-        this.movingZombieLocation.x += this.gridDimensions
-      } else if (this.movingZombieLocation.y < 0) {
-        this.movingZombieLocation.y += this.gridDimensions
-      } else if (this.movingZombieLocation.x > this.gridDimensions) {
-        this.movingZombieLocation.x -= this.gridDimensions
-      } else if (this.movingZombieLocation.y > this.gridDimensions) {
-        this.movingZombieLocation.y -= this.gridDimensions
-      }
-      // check if in creature location
-      if (this.creatureLocations.includes(this.movingZombieLocation)) {
-
-      }
-    })
+        // check if zombie has gone out of bounds and correct location if it has
+        if (zombie.location[0] < 0) {
+          zombie.location[0] += this.gridDimensions
+        } else if (zombie.location[1] < 0) {
+          zombie.location[1] += this.gridDimensions
+        } else if (zombie.location[0] > this.gridDimensions - 1) {
+          zombie.location[0] -= this.gridDimensions
+        } else if (zombie.location[1] > this.gridDimensions - 1) {
+          zombie.location[1] -= this.gridDimensions
+        }
+        console.log(`Zombie ${zombieIndex} moved to: ${zombie.location}`)
+        // check if in creature location
+        if (this.creatureLocations.some((creatureLocation) => arraysEqual(creatureLocation, zombie.location))) {
+          console.log(`Creature infected at ${zombie.location}`)
+          this.createZombie(zombie.location)
+          const filtered = this.creatureLocations.filter(creatureLocation => !arraysEqual(creatureLocation, zombie.location))
+          this.creatureLocations = filtered
+        }
+      })
+      zombieIndex++
+    }
+    console.log("Zombie positions:")
+    this.zombies.forEach((zombie) => console.log(zombie.location))
+    console.log("Creature positions:")
+    if (this.creatureLocations.length) {
+      this.creatureLocations.forEach((creatureLocation) => console.log(creatureLocation))
+    } else {
+      console.log("none")
+    }
   }
+}
+
+function arraysEqual(a: Location, b: Location): boolean {
+  if (a === b) return true;
+  if (a == null || b == null) return false;
+  if (a.length !== b.length) return false;
+
+  for (let i = 0; i < a.length; ++i) {
+    if (a[i] !== b[i]) return false;
+  }
+  return true;
 }
